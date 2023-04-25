ef48dafbdf2fbc7eea2a00cfab77e0b5d5bf2b65
Jck123 <jck.1.2.3.a@gmail.com>
2023-04-25 13:58:26 -0400

    created database creation script

---
+++ sql/g18finalproj.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS g18finalproj;
+
+CREATE DATABASE g18finalproj;
+
+USE g18finalproj;
+
+CREATE TABLE user (
+    name            VARCHAR(64),
+    password        VARCHAR(64),
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    bio             TINYTEXT
+);
+
+CREATE TABLE post (
+    contents        TEXT(65535),
+    id              INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE board (
+    name            VARCHAR(64),
+    description     TINYTEXT,
+    id              INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE author (
+    user_id         INT,
+    post_id         INT,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (post_id) REFERENCES post(id)
+);
+
+CREATE TABLE post_location (
+    post_id         INT,
+    board_id        INT,
+    FOREIGN KEY (post_id) REFERENCES post(id),
+    FOREIGN KEY (board_id) REFERENCES board(id)
+);
